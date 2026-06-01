5c04835fb213febe70302d1dadd71183b9d5de1f
Bishnu Upadhyay <bishnuupadhyay012@gmail.com>
2026-05-31 23:12:12 -0400

    image added in list view

--- js/menu.js
+++ js/menu.js
@@ -43,6 +43,87 @@ function loadMenu() {
     .catch(err => console.error("Menu load error:", err));
 }
 
+
+let allData = [];
+let activeCategory = "All";
+
+function loadMenu() {
+  fetch("menu.json")
+    .then(res => res.json())
+    .then(data => {
+      allData = data;
+
+      renderTabs(data);
+      renderMenu(data);
+
+      console.log("Menu loaded once");
+    })
+    .catch(err => console.error("Menu load error:", err));
+}
+
+function renderTabs(data){
+  const tabs = document.getElementById("menuTabs");
+
+  const categories = ["All", ...data.map(d => d.category)];
+
+  tabs.innerHTML = "";
+
+  categories.forEach(cat => {
+    const btn = document.createElement("button");
+    btn.className = "tab-btn";
+    btn.innerText = cat;
+
+    if(cat === activeCategory) btn.classList.add("active");
+
+    btn.addEventListener("click", () => {
+      activeCategory = cat;
+      renderMenu(allData);
+      renderTabs(allData);
+    });
+
+    tabs.appendChild(btn);
+  });
+}
+function renderMenu(data){
+  const container = document.getElementById("menuContainer");
+  container.innerHTML = "";
+
+  data.forEach(section => {
+
+    if(activeCategory !== "All" && section.category !== activeCategory){
+      return;
+    }
+
+    const card = document.createElement("div");
+    card.className = "menu-card";
+
+    let itemsHTML = "";
+
+    section.items.forEach(item => {
+      itemsHTML += `
+        <a href="details.html?id=${item.id}" class="food-link">
+          <div class="item">
+            
+            <div class="item-left">
+              <img src="./images/${item.image}" alt="${item.name}">
+              <span>${item.name}</span>
+            </div>
+
+            <span>$${item.price}</span>
+
+          </div>
+        </a>
+      `;
+    });
+
+    card.innerHTML = `
+      <h3>${section.category}</h3>
+      ${itemsHTML}
+    `;
+
+    container.appendChild(card);
+  });
+}
 /* =========================
    CHEFS LOADING
 ========================= */
