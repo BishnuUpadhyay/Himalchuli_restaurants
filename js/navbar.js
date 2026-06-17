ba3c253bf01f2638f0e84c44558bca4b303db7e7
Bishnu Upadhyay <bishnuupadhyay012@gmail.com>
2026-06-17 17:37:18 -0400

    New version

--- js/navbar.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.getElementById("navToggle");
-const nav = document.getElementById("navLinks");
-
-toggle.addEventListener("click", () => {
-    console.log("Clicked")
-  nav.classList.toggle("show");
-});
